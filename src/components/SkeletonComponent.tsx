ad688dd969b400f9a330028fe452c29af6ff133b
adasilvapdev <adasilvapdev@gmail.com>
2021-08-17 12:09:36 -0400

    Add the animation for skeleton in gray scale

--- src/components/SkeletonComponent.tsx
+++ src/components/SkeletonComponent.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { StyleSheet, View, Dimensions, Animated, Easing } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient';
+const { width } = Dimensions.get('window')
+const AnimatedLG = Animated.createAnimatedComponent(LinearGradient);
 
-const SkeletonComponent = () => {
+const SkeletonComponent = (props: any) => {
+    const animatedValue = new Animated.Value(0);
+    const translateX = animatedValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: [-width, width],
+    })
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.timing(animatedValue, {
+                toValue: 1,
+                duration: 1000,
+                // @ts-ignore
+                easing: Easing.linear.inOut,
+                useNativeDriver: true,
+            })
+        ).start();
+    }, []);
+
     return (
-        <View>
-            <Text></Text>
+        <View style={{
+            backgroundColor: '#a0a0a0',
+            borderColor: '#b0b0b0',
+            height: 150,
+            width
+        }}>
+            <AnimatedLG
+                colors={['#a0a0a0', '#b0b0b0', '#b0b0b0', '#a0a0a0',]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+                style={{
+                    ...StyleSheet.absoluteFillObject,
+                    transform: [{ translateX }],
+                }}
+
+            />
+
         </View>
     )
 }
