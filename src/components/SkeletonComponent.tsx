7c805e0994438268f5384c0ab2451efffe21e992
adasilvapdev <adasilvapdev@gmail.com>
2021-08-17 11:18:13 -0400

    Create the skeleton component

---
+++ src/components/SkeletonComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SkeletonComponent = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default SkeletonComponent
+
+const styles = StyleSheet.create({})
